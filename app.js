32b148e8c0614afa6a97aa6f6ef9f8e1612bf468
The Good Maxwell <maxwellraden@gmail.com>
2017-02-09 11:23:43 -0600

    basic functionalit and clickable images

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 $(document).ready(function() {
 	const APIKEY = "AIzaSyAOlN6_KVX0PqPLsaA6raMgHhyA8DeX5Hw";
 	const URL = "https://www.googleapis.com/youtube/v3/search";
-	const EMBED = "https://www.youtube.com/embed";
+	const EMBED = "https://www.youtube.com/embed/";
+	let token;
 
 	$("#search-form").submit(event => {
 		event.preventDefault();
@@ -11,17 +12,30 @@ $(document).ready(function() {
 		getRequest(query);
 	});
 
-	const getRequest = (query) => {
+	const getRequest = (query, token) => { 
 		let params = {
 			part: 'snippet',
 			key: APIKEY,
 			q: query,
-			type: 'video'
+			type: 'video',
+			pageToken: token
 		};
 		
 		$.getJSON(URL, params, data => {
+			for(var i =0; i < data.items.length; i++) {
+				var thumbnail = data.items[i].snippet.thumbnails.default.url;
+				var clickable = EMBED + data.items[i].id.videoId;
+				var html = "";
+				html += "<div>";
+				html += "<h3>" + data.items[i].snippet.title + "</h3>";
+				html += `<a href='${clickable}'> <img src='${thumbnail}'> </a>`
+				$("#results").append(html);
+			}
+			 var token = data.nextPageToken;
+
 			console.log(data);
 		});
+
 	} 
 
 });
